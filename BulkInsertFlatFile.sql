d58ffd50c99484cabb39fff86a32e3ccd8776547
EdmundCPortfolio <149627639+EdmundCPortfolio@users.noreply.github.com>
2024-09-23 21:17:29 +0100

    Update BulkInsertFlatFile.sql

--- BulkInsertFlatFile.sql
+++ BulkInsertFlatFile.sql
@@ -1,3 +1,4 @@
+--Create landing table
 CREATE TABLE DowJonesDailyClose (
     [Date] VARCHAR(19),
     [Open] FLOAT,
@@ -7,28 +8,28 @@ CREATE TABLE DowJonesDailyClose (
 );
 
 
---Bulk inset a csv file
+--Example 1, Bulk inset a csv file
 BULK INSERT DowJonesDailyClose 
-FROM 'E:\Work Files\SQL\Database Code\BulkInsert\DowJonesData\DJIA.csv'
+FROM 'E:\SQL\Database Code\BulkInsert\DowJonesData\DJIA.csv'
 WITH (
     FIELDTERMINATOR = ',',
     ROWTERMINATOR = '\n',
     FIRSTROW = 2, -- Skip the header row
     BATCHSIZE = 100000, -- Process 100,000 records per batch
-	CODEPAGE = '65001' -- Use UTF-8 encoding
+    CODEPAGE = '65001' -- Use UTF-8 encoding
     
 );
 
 
---Bulk inset a text file, pipe delimited
+--Example 2, Bulk inset a text file, pipe delimited
 BULK INSERT DowJonesDailyClose 
-FROM 'E:\Work Files\SQL\Database Code\BulkInsert\DowJonesData\DJIA_Pipe.txt'
+FROM 'E:\SQL\Database Code\BulkInsert\DowJonesData\\DJIA_Pipe.txt'
 WITH (
     FIELDTERMINATOR = '|',
     ROWTERMINATOR = '\n',
     FIRSTROW = 2, -- Skip the header row
     BATCHSIZE = 100000, -- Process 100,000 records per batch
-	CODEPAGE = '65001' -- Use UTF-8 encoding
+    CODEPAGE = '65001' -- Use UTF-8 encoding
     
 );
 
